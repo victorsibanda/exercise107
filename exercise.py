a7b27cbbd6ad9623ec890379055fc7bc96ee89e3
Victor Sibanda <vsibanda@tcadmins-MacBook-Air.local>
2020-03-04 10:17:51 +0000

    changed code to format food order as a list

--- exercise.py
+++ exercise.py
@@ -2,14 +2,11 @@
 
 # User Stories
 #1
-# AS a User I want to be able to see the menu in a formated way, so that I can order my meal.
-
+# AS a User I want to be able to see the menu in a formated way, so that I can order my meal
 #2
 # AS a User I want to be able to order 3 times, and have my responses added to a list so they aren't forgotten
-
 #3
 # As a user, I want to have my order read back to me in formated way so I know what I ordered.
-
 # DOD
 # its own project on your laptop and Github
 # be git tracked
@@ -27,10 +24,18 @@ menu = ['falafel', 'HuMMus', 'coUScous', 'Bacalhau a Bras']
 #print(menu[2].title())
 #print(menu[3].title())
 
-print ('Menu Items:')
+
 new_menu = [menu[0].title(),menu[1].title(),menu[2].title(),menu[3].title()]
 
 print(f'here is your formatted menu{new_menu}')
+
+print ('Menu Items:')
+print('1.',new_menu[0])
+print('2.',new_menu[1])
+print('3.',new_menu[2])
+print('4.',new_menu[3])
+
+
 
 food_order = []
 
@@ -43,7 +48,10 @@ food_order.append(input('What would you like to order second?').strip().capitali
 food_order.append(input('What would you like to order third?').strip().capitalize())
 #print(food_order)
 
-print(f'You would like {food_order}')
+print(f'You would like :')
+print('1.',food_order[0])
+print('2.',food_order[1])
+print('3.',food_order[2])
 
 
 # I need to print each item from the list
